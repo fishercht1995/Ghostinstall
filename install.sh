ed022da9c86121b2454ac1a80e3f250933c7a5d7
fishercht1995 <45677828+fishercht1995@users.noreply.github.com>
2021-11-24 18:13:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
 apt update -y    
-sudo apt-get install imagemagick graphviz dvipng python3-venv fonts-dejavu fonts-noto-cjk latexmk librsvg2-bin texlive-xetex
+sudo apt-get install imagemagick graphviz dvipng python3-venv fonts-dejavu fonts-noto-cjk latexmk librsvg2-bin texlive-xetex -y
 apt update -y
-sudo apt install apt-transport-https curl gnupg
+sudo apt install apt-transport-https curl gnupg -y
 sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-sudo apt update && sudo apt install bazel
-sudo apt update && sudo apt full-upgrade
-apt install clang
-apt install jfsutils
-apt install reiserfsprogs
-apt install xfsprogs
-apt install squashfs-tools
-apt install btrfs-progs
-apt install quota
-apt install ppp
-apt install oprofile
-apt install udev
-apt install grub
-apt install mcelog
-apt-get install python3-sphinx
+sudo apt update && sudo apt install bazel -y
+sudo apt update && sudo apt full-upgrade -y
+apt install clang -y
+apt install jfsutils -y
+apt install reiserfsprogs -y
+apt install xfsprogs -y
+apt install squashfs-tools -y
+apt install btrfs-progs -y
+apt install quota -y
+apt install ppp -y
+apt install oprofile -y
+apt install udev -y
+apt install grub -y
+apt install mcelog -y
+apt-get install python3-sphinx -y
 sudo apt-get update -y
 sudo apt-get install -y udev
-apt install libelf
-apt install libelf-dev
+apt install libelf -y
+apt install libelf-dev -y
 
